297799ee16d3ea972a2d9bb8f974fb5486d1ed9b
DmitryFreeLance <dmitryshagaev@bk.ru>
2024-10-03 01:18:00 +0700

    done

--- ToDoList/src/script.js
+++ ToDoList/src/script.js
@@ -10,8 +10,51 @@ document.addEventListener("DOMContentLoaded", function() {
             button.onclick = function(e) {
                 const item = e.target.parentNode;
                 item.remove();
-                currentHeight -= 31;
+                currentHeight -= 40;
                 content.style.height = `${currentHeight}px`;
+            };
+        });
+    }
+
+    function findEditButtons() {
+        const editButtons = document.querySelectorAll(".edit-btn");
+
+        editButtons.forEach(function(button) {
+            button.onclick = function(e) {
+                const parent = button.parentElement;
+                const textElement = parent.querySelector(".text");
+
+                const originalText = textElement.innerText;
+
+                textElement.innerHTML = `<input type="text" id="edit-input" value="${originalText}" maxlength="52">`;
+
+                button.outerHTML = `
+                    <button class="save-btn">Сохранить</button>
+                    <button class="cancel-btn">Отменить</button>
+                `;
+
+                const saveButton = parent.querySelector(".save-btn");
+                const cancelButton = parent.querySelector(".cancel-btn");
+                const inputField = parent.querySelector("#edit-input");
+
+                saveButton.onclick = function() {
+                    const newText = inputField.value;
+                    textElement.innerHTML = newText;
+
+                    saveButton.outerHTML = `<button class="edit-btn">Редактировать</button>`;
+                    cancelButton.remove();
+
+                    findEditButtons();
+                };
+
+                cancelButton.onclick = function() {
+                    textElement.innerHTML = originalText;
+
+                    cancelButton.outerHTML = `<button class="edit-btn">Редактировать</button>`;
+                    saveButton.remove();
+
+                    findEditButtons();
+                };
             };
         });
     }
@@ -22,15 +65,25 @@ document.addEventListener("DOMContentLoaded", function() {
         if (input.length == 0) {
             alert('Необходимо ввести данные!');
         } else {
-            list.innerHTML += '<div><ul class="spisok">' + input + '<button class="edit-btn">Редактировать</button> <button class="remove-btn">Удалить</button></ul></div>';
+            list.innerHTML += `
+                <div>
+                    <ul class="spisok">
+                        <span class="text">${input}</span>
+                        <button class="edit-btn">Редактировать</button>
+                        <button class="remove-btn">Удалить</button>
+                    </ul>
+                </div>
+            `;
             document.querySelector("#input").value = '';
 
-            currentHeight += 31;
+            currentHeight += 40;
             content.style.height = `${currentHeight}px`;
 
+            findEditButtons();
             findRemoveButtons();
         }
     };
 
+    findEditButtons();
     findRemoveButtons();
 });
