2ce4b7330e65c31ccdfd7730baacecdcce31dbd3
DmitryFreeLance <dmitryshagaev@bk.ru>
2024-11-06 18:10:52 +0700

    EXPRESS JS COMEBACK

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
